40b759f66d7a82fdde6c814c78d49a89dd2c8b60
Rag2118 <iamraghul21@gmail.com>
2026-07-10 20:46:58 +0530

    "Completed Java-Script Introduction

---
+++ JAVASCRIPT/1-Introduction/script.js
@@ -0,0 +1,9 @@
+console.log("Hello Javascript");
+console.log("Learning JavaScript");
+console.log("First JavaScript program");
+
+console.log(21);
+console.log(true);
+console.log(false);
+
+console.log("JavaScript executes line by line");
