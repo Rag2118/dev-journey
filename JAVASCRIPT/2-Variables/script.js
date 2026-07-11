dea483e64cedfd356d13269b2510336e2c211ae7
Rag2118 <iamraghul21@gmail.com>
2026-07-11 11:29:52 +0530

    Complete JavaScript Variables with practice

---
+++ JAVASCRIPT/2-Variables/script.js
@@ -0,0 +1,26 @@
+const collegeName="GRD College of Science";
+let studentName="Raghul";
+let age=20;
+let city="Tirupur";
+let course="BSC IT";
+console.log(collegeName);
+console.log(studentName);
+console.log(age);
+console.log(city);
+console.log(course);
+age=21;
+city="Cbe";
+console.log(age);
+console.log(city);
+{
+    let section="A";
+    const year=3;
+
+    console.log(section);
+    console.log(year);
+}
+//console.log(section);
+{
+    var department="IT";
+}
+console.log(department);
