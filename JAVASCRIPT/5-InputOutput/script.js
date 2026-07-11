ab58d64f96b1408a337d4905af7c7471ad3bfac0
Rag2118 <iamraghul21@gmail.com>
2026-07-11 21:06:09 +0530

    Complete JavaScript Input & Output

---
+++ JAVASCRIPT/5-InputOutput/script.js
@@ -0,0 +1,11 @@
+let studentName=prompt("Enter your Name:");
+let age=Number(prompt("Enter your Age:"));
+let city=prompt("Enter your city:");
+let course=prompt("Enter your course:");
+let result=confirm("Do you want to Submit?");
+console.log("=====STUDENT DETAILS=====")
+console.log("StudentName:",studentName);
+console.log("Age:",age);
+console.log("City:",city);
+console.log("Course:",course);
+console.log(result);
