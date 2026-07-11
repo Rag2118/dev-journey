1f4bd47066cf02cb2a4f3c8eb9a4506393f4ab6f
Rag2118 <iamraghul21@gmail.com>
2026-07-11 14:32:23 +0530

    Complete JavaScript data types

---
+++ JAVASCRIPT/3-DataTypes/script.js
@@ -0,0 +1,27 @@
+let name="Raghul";
+let age=21;
+let hasLicense=false;
+let homeAddress=null;
+let bankBalance=1097776548907892876345n;
+let id=Symbol("id");
+let user={};
+user[id]=101;
+let mobile={
+    brand:"Iphone",
+    model:"17 Pro Max",
+    price:200000
+};
+console.log(name);
+console.log(typeof name);
+console.log(age);
+console.log(typeof age);
+console.log(hasLicense);
+console.log(typeof hasLicense);
+console.log(homeAddress);
+console.log(typeof homeAddress);
+console.log(bankBalance);
+console.log(typeof bankBalance);
+console.log(user[id]);
+console.log(typeof id);
+console.log(mobile);
+console.log(typeof mobile);
