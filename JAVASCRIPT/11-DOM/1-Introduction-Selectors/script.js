b1f5779955cab973a5fae3815aaf0a76cf291b3a
Rag2118 <iamraghul21@gmail.com>
2026-07-15 20:52:31 +0530

    Complete DOM introduction and selectors

---
+++ JAVASCRIPT/11-DOM/1-Introduction-Selectors/script.js
@@ -0,0 +1,7 @@
+console.log(document.getElementById("title"));
+console.log(document.getElementsByClassName("text"));
+console.log(document.getElementsByTagName("p"));
+console.log(document.querySelector("#title"));
+console.log(document.querySelector(".text"));
+console.log(document.querySelector("button"));
+console.log(document.querySelectorAll(".text"));
